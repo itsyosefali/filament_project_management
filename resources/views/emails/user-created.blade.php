a3600ef5b3f713006667d29e5a54e29026ec5afd
itsyosefali <joeyxjoey123@gmail.com>
2025-05-04 09:23:25 +0200

    feat: enhance user-created email template with localized content and improved guidance

--- resources/views/emails/user-created.blade.php
+++ resources/views/emails/user-created.blade.php
@@ -1,18 +1,28 @@
 @component('mail::message')
-# Welcome, {{ $user->name }}!
 
-We (اتحاد طلبة كلية التقنية الإلكترونية) are excited to have you on board!
-Your account is ready, and we can’t wait to see you in action.
+<img src="{{ asset('images/logocuet.png') }}" alt="شعار الاتحاد" width="80" style="display: block; margin: 0 auto 20px;">
+
+# مرحباً {{ $user->name }}!
+
+يسرنا انضمامك إلى اتحاد طلبة كلية التقنية الإلكترونية وتهانينا على إنشاء حسابك الجديد. بانضمامك إلينا، نسعى سويًا إلى:
+- تطوير مهاراتنا الأكاديمية
+- تعزيز أواصر التعاون والتواصل بين الطلبة
 
 @component('mail::button', ['url' => url('/')])
-Join Us Now
+ابدأ الآن
 @endcomponent
 
 @component('mail::panel')
-**Pro Tip:** Personalize your profile and explore our resources to get the most out of our platform.
+**نصيحة هامة:**
+- قم بإكمال ملفك الشخصي.
+- غيّر كلمة المرور الافتراضية للحصول على أفضل استفادة.
 @endcomponent
 
-Best Regards,
+نتمنى لك التوفيق والنجاح،
 {{ config('app.name') }}
+
+إذا كان لديك أي استفسارات أو تواجه أي مشكلة، يمكنك التواصل مع:
+[@et_dev](https://t.me/et_dev)
+
 @endcomponent
 
